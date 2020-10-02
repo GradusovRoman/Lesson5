a9060c46370291c83bd488bcd4ee818d928fc8db
GradusovRoman <monomari86@gmail.com>
2020-10-02 20:33:15 +0500

    Lesson5 try #1

---
+++ EmployeeData.java
@@ -0,0 +1,35 @@
+package geekbrains.lesson5_homework_oop;
+
+public class EmployeeData {
+    private String name;
+    private String post;
+    private String email ;
+    private String phoneNumber;
+    private int wage;
+    private int age;
+
+    EmployeeData (String name,String post,String email,String phoneNumber,int wage,int age){
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.phoneNumber=phoneNumber;
+        this.wage=wage;
+        this.age = age;
+        }
+
+
+    int GetAge() {
+        return age;
+    }
+
+    void  Show() {
+       System.out.println(name + "\n-"+
+               "post: "+post + "\n-"+
+               "email: " +email + "\n-"+
+               "phoneNumber: "+phoneNumber + "\n-"+
+               "wage: "+wage + "\n-"+
+               "age: "+age);
+       System.out.println();
+
+    }
+    }
---
+++ Lesson5_oop.java
@@ -0,0 +1,30 @@
+package geekbrains.lesson5_homework_oop;
+
+import javax.swing.*;
+
+public class Lesson5_oop {
+
+    public static void main(String[] args) {
+        EmployeeData[] persArray = new EmployeeData[5];
+        persArray[0] = new EmployeeData("Gradusov Roman", "Java Developer", "monomari86@gmail.com",
+                "89639007700", 120000, 33);
+        persArray[1] = new EmployeeData("Rudenko Ivan", "Engineer", "ivivan1@mailbox.com",
+                "89639007701", 60000, 41);
+        persArray[2] = new EmployeeData("Ivanov Alex", "Manager", "AI_box_wrk@mail.ru",
+                "89639007702", 50000, 35);
+        persArray[3] = new EmployeeData("Mikulin Semen", "System Administrator", "ivivan3@mailbox.com",
+                "89639007703", 65000, 34);
+        persArray[4] = new EmployeeData("Hlebnikov  Maxim", "Engineer", "ivivan4@mailbox.com",
+                "73472465123", 75000, 45);
+
+        System.out.println("Список сотрдуников старше 40 лет:");
+
+        for (EmployeeData employee : persArray) {
+          if (employee.GetAge() > 40)  employee.Show();
+
+            }
+        }
+
+        }
+
+
